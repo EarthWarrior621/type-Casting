9578727a4ce7c07bb42b75b4fdb9cce3f0cc8816
William Garcia <114087502+EarthWarrior621@users.noreply.github.com>
2023-03-09 09:43:08 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace type_Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks user to enter a wholenumer and assigns input to integer
+            Console.WriteLine("Please enter a whole naumber!");
+            int myFavNum = Convert.ToInt32(Console.ReadLine());
+
+            // Prints favorite number to console
+            Console.WriteLine("Your favortite number is " + myFavNum);
+
+            //Asks user a true/false question and assigns response to a boolean
+            Console.WriteLine("do you like video games? (Please enter 'true' or 'false')");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+
+
+            // Print the result of question asked
+            Console.WriteLine($"It is {isTrue} that I like video games!");
+        }
+    }
+}
